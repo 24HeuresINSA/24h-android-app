9787eeabeefea09e221604a1008111be43f48bac
Remi Pichon <pichon.remi.pr@gmail.com>
2015-04-29 17:38:15 +0200

    [RPI] #47 : add tabs

--- app/src/main/java/com/insalyon/les24heures/NightActivity.java
+++ app/src/main/java/com/insalyon/les24heures/NightActivity.java
@@ -1,15 +1,27 @@
 package com.insalyon.les24heures;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.insalyon.les24heures.fragments.ArtistFragment;
+import com.insalyon.les24heures.view.SlidingTabLayout;
+
+import butterknife.InjectView;
 
 /**
  * Created by remi on 12/03/15.
  */
 public class NightActivity extends BaseDynamicDataActivity {
+
+    @InjectView(R.id.sliding_tabs)
+    SlidingTabLayout mSlidingTabLayout;
+    @InjectView(R.id.view_pager)
+    ViewPager mViewPager;
+    private OurViewPagerAdapter mViewPagerAdapter;
 
 
 
@@ -24,26 +36,52 @@ public class NightActivity extends BaseDynamicDataActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
+        mViewPagerAdapter = new OurViewPagerAdapter(getFragmentManager());
+        mViewPager.setAdapter(mViewPagerAdapter);
+
+
+        mSlidingTabLayout.setCustomTabView(R.layout.tab_indicator, android.R.id.text1);
+
+        mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.tab_selected_strip));
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setViewPager(mViewPager);
+
+        mSlidingTabLayout.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                restoreTitle();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
         clearDrawerChoices();
         artistButton.setActivated(true);
         setSelectedMenuItem(artistButton, R.drawable.concert_bleu);
         drawerLayout.closeDrawer();
-        startFragment();
+        //startFragment();
         restoreTitle();
     }
 
 
-    private void startFragment() {
+    private ArtistFragment getArtistFragment() {
         Bundle bundleArgs = new Bundle();
         bundleArgs.putParcelableArrayList("categoriesSelected", selectedCategories);
         bundleArgs.putParcelableArrayList("resourcesList", nightResourceArrayList);
         searchQuery = (searchQuery == null) ? null : (searchQuery.equals("")) ? null : searchQuery;
         bundleArgs.putString("searchQuery", searchQuery);
 
-        Fragment fragment = new ArtistFragment();
+        ArtistFragment fragment = new ArtistFragment();
         fragment.setArguments(bundleArgs);
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.replace(R.id.content_frame, fragment).commit();
+
+        return fragment;
     }
 
     @Override
@@ -54,4 +92,30 @@ public class NightActivity extends BaseDynamicDataActivity {
             setTitle(str);
         }
     }
+
+    private class OurViewPagerAdapter extends FragmentPagerAdapter {
+
+        public OurViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            return getArtistFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) return getResources().getString(R.string.dayTabmap);
+            return getResources().getString(R.string.dayTablist);
+        }
+    }
+
+
 }
